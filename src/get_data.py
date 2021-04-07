cdda2df78a91652d39af15bf6f711cec3c6ecf63
Tusharroul <tushar.roul@gmail.com>
2021-04-07 19:26:07 +0530

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -16,6 +16,8 @@ def get_data(config_path):
     # print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
+    print("test")
+    print(df)
     return df
 
 
